66d11bf87ed7d5fa3894834a0ece46adfd7b31f0
ChronoTrigger64 <sydneey@hawaii.edu>
2024-03-31 17:18:55 -1000

    added in player movement animation
    Added in player sprites, made a new folder for it.  Also moved all sprites assets to the Sprites folder.
    Added:
    idle, run, and jump animations.
    
    also edited the Player Movement script to add these animations in.
    
    Fixed infinite jump; player can only jump 1 time.

--- Cosmic Hackdown/Assets/Scripts/PlayerMovement.cs
+++ Cosmic Hackdown/Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,9 @@ public class PlayerMovement : MonoBehaviour
 {
 
     private Rigidbody2D body;
+    private Animator anim;
+
+    private bool grounded;
 
     //SerializeField allows you to edit the speed directly from Unity
    [SerializeField] private float speed;
@@ -16,11 +19,14 @@ public class PlayerMovement : MonoBehaviour
         //player has type rigidbody2d as you can see in its inspector so it will take its rigidbody2d
         //and store into the body variable
         body = GetComponent<Rigidbody2D>();
+        //reference for animator component
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     private void Update()
     {
+        float horizontalInput = Input.GetAxis("Horizontal");
         /*Vector3 is a collection of numbers to assign speed in all 3 directions:
         1. left and right
         2. up and down
@@ -38,9 +44,10 @@ public class PlayerMovement : MonoBehaviour
         This is to make your character JUMP
         KeyCode is an enumeration that contains all buttons.  
         Use KeyCode.Enter to check if enter was pressed, etc.  In this case, we check
-        if space was pressed or not.
+        if space was pressed or not.  It also checks if it's grounded.
+        character will only jump if it is on the ground, making it so you can't do infinite jumps.
         */
-        if(Input.GetKey(KeyCode.Space)) {
+        if(Input.GetKey(KeyCode.Space) && grounded) {
             /*
             So if space bar is pressed, we do the following below:
             set the body's velocity to the speed variable and only update this for the y axis.
@@ -48,7 +55,26 @@ public class PlayerMovement : MonoBehaviour
             This is to make your character JUMP up (which is why we need to change the y axis
             but not the x axis)
             */
-            body.velocity = new Vector2(body.velocity.x, speed);
+            Jump();
+        }
+
+        //Set animator parameters
+        //is 0 not equal to 0? false.  so it plays the idle animation.  But if there is horizontal input, this will evaluate to true.
+        anim.SetBool("run", horizontalInput != 0);
+        anim.SetBool("grounded", grounded);
+    }
+
+    private void Jump() {
+
+        body.velocity = new Vector2(body.velocity.x, speed);
+        grounded = false;
+
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if(collision.gameObject.tag == "Ground") {
+            grounded = true;
         }
     }
 
