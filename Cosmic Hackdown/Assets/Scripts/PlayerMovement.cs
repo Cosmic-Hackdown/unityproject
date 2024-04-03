3765ee528ff6fa978c95227d2fd6338190c739df
ChronoTrigger64 <sydneey@hawaii.edu>
2024-04-02 23:48:12 -1000

    changed jump height to speed*1.5 as opposed to just speed
    jump height wasn't high enough.  I changed it.

--- Cosmic Hackdown/Assets/Scripts/PlayerMovement.cs
+++ Cosmic Hackdown/Assets/Scripts/PlayerMovement.cs
@@ -66,7 +66,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void Jump() {
 
-        body.velocity = new Vector2(body.velocity.x, speed);
+        body.velocity = new Vector2(body.velocity.x, speed*1.5f);
         grounded = false;
 
 
