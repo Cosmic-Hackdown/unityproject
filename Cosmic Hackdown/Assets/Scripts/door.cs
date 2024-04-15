7099ac21a82b4340640dbd35d76a7057404121a8
Matthew Fallah (マシュー・ファラー) <107450287+moi-cest-matthew@users.noreply.github.com>
2024-04-14 18:11:03 -1000

    Door animation working, but need to fix bug where it immediately recloses

--- Cosmic Hackdown/Assets/Scripts/door.cs
+++ Cosmic Hackdown/Assets/Scripts/door.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class door : MonoBehaviour
 {
     public bool isLocked;
+    private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         isLocked = true;
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,6 +23,7 @@ public class door : MonoBehaviour
     {
         if(other.gameObject.CompareTag("Key"))
         {
+            anim.SetTrigger("Open");
             isLocked = false;
             Debug.Log("Débloqué");
         }
@@ -30,6 +33,7 @@ public class door : MonoBehaviour
     {
         if(other.gameObject.CompareTag("Key"))
         {
+            anim.SetTrigger("Close");
             isLocked = true;
             Debug.Log("Bloqué");
         }
