15dd435969c6d6df1dfd229746051f801af52314
Matthew Fallah (マシュー・ファラー) <107450287+moi-cest-matthew@users.noreply.github.com>
2024-04-16 20:53:07 -1000

    Sacrifice door animation to make transition to level2 work

--- Cosmic Hackdown/Assets/Scripts/door.cs
+++ Cosmic Hackdown/Assets/Scripts/door.cs
@@ -1,16 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class door : MonoBehaviour
 {
-    public bool isLocked;
-    private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        isLocked = true;
-        anim = GetComponent<Animator>();
+       
     }
 
     // Update is called once per frame
@@ -23,19 +21,7 @@ public class door : MonoBehaviour
     {
         if(other.gameObject.CompareTag("Key"))
         {
-            anim.SetTrigger("Open");
-            isLocked = false;
-            Debug.Log("Débloqué");
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if(other.gameObject.CompareTag("Key"))
-        {
-            anim.SetTrigger("Close");
-            isLocked = true;
-            Debug.Log("Bloqué");
+            SceneManager.LoadScene("Level2");
         }
     }
 }
